8fb0e28e106882da26cf6a1f6f4d9c25195ae29b
nguyennga26172004 <nguyennga2617@gmail.com>
2025-03-20 18:24:41 +0700

    nga

--- Router/web.php
+++ Router/web.php
@@ -4,8 +4,8 @@ $act = isset($_GET['act']) ? $_GET['act'] : "";
 
 switch($act){
     case ''; {
-        $dashBoardController = new DashboardController();
-        $dashBoardController->Dashboar();
+        $dashBoardController = new ProductController();
+        $dashBoardController->showAllproduct();
 
         break;
     }

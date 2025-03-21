3f52b19d40bf2e6f4c4993e7cb408363e26c5d4d
dinhvn224 <hhit357@gmail.com>
2025-03-21 19:04:22 +0700

    edit admin  product client

--- App/Views/Admin/products.php
+++ App/Views/Admin/products.php
@@ -17,20 +17,22 @@
     <!-- Mobile Specific Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    <!-- Theme Style -->
-    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
-    <link rel="stylesheet" type="text/css" href="css/animation.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+    <link rel="stylesheet" type="text/css" href="/DA_1/Douong_Pony/Assets/Admin/css/animate.min.css">
+<link rel="stylesheet" type="text/css" href="/DA_1/Douong_Pony/Assets/Admin/css/animation.css">
+<link rel="stylesheet" type="text/css" href="/DA_1/Douong_Pony/Assets/Admin/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="/DA_1/Douong_Pony/Assets/Admin/css/bootstrap-select.min.css">
+<link rel="stylesheet" type="text/css" href="/DA_1/Douong_Pony/Assets/Admin/css/styles.css">
+
+
 
 
 
     <!-- Font -->
-    <link rel="stylesheet" href="font/fonts.css">
+    <link rel="stylesheet" href="/DA_1/Douong_Pony/Assets/Admin/font/fonts.css">
 
     <!-- Icon -->
-    <link rel="stylesheet" href="icon/style.css">
+    <link rel="stylesheet" href="/DA_1/Douong_Pony/Assets/Admin/icon/style.css">
 
     <!-- Favicon and Touch Icons  -->
     <link rel="shortcut icon" href="images/favicon.png">
